ec9fc67a2b06c2c9b239bf10de33cdd0719dd14e
liang <guoliang1222@gmail.com>
2016-11-26 16:45:51 +0200

    done the websocket ui side

--- frontend/www/js/controllers.js
+++ frontend/www/js/controllers.js
@@ -39,31 +39,59 @@ angular.module('starter.controllers', [])
     'hi'
   ];
 
+  var that = this;
+
+  window.WebSocket = window.WebSocket || window.MozWebSocket;
+  window.ws = new WebSocket('ws://193.166.24.212:1337');
+  ws.onopen = function () {
+    // ws is opened and ready to use
+    console.log('ws open')
+  };
+
+  ws.onerror = function (error) {
+    // an error occurred when sending/receiving data
+    console.log('ws error', error);
+  };
+
+  ws.onmessage = function (message) {
+    var json = JSON.parse(message.data);
+      console.log(json);
+      console.log('that', this);
+      console.log('scope', $scope);
+      $scope.messages.push(json.text);
+      if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
+          $scope.$apply();
+      }
+  };
+
   $scope.add = function (text) {
     $scope.messages.push(text);
     var detection = $scope.detect(text);
-    var id = detection.trim();
-    if (id) {
-      var device = Chats.getDevice(id);
-      if (device) {
-        Chats.fetchDeviceRemotely(device.data_source).then(function (data) {
-          console.log('desk data', data);
-          if (data.data.measurements.value) {
-            var message = id + ' Occupied now!';
-            $scope.messages.push(message);    
-          } else {
-            var message = id + ' FREE now!';
-            $scope.messages.push(message);    
-          }
-        }, function () {
-          var message = 'Sorry, i can not get' + ' ' + id + 'information now';
-          $scope.messages.push(message);  
-        })
-      } else {
-        var message = 'Sorry, i can not find' + ' ' + id;
-        $scope.messages.push(message);
+    if (detection) {
+      var id = detection.trim();
+      if (id) {
+        var device = Chats.getDevice(id);
+        if (device) {
+          Chats.fetchDeviceRemotely(device.data_source).then(function (data) {
+            console.log('desk data', data);
+            if (data.data.measurements.value) {
+              var message = id + ' Occupied now!';
+              $scope.messages.push(message);    
+            } else {
+              var message = id + ' FREE now!';
+              $scope.messages.push(message);    
+            }
+          }, function () {
+            var message = 'Sorry, i can not get' + ' ' + id + 'information now';
+            $scope.messages.push(message);  
+          })
+        } else {
+          var message = 'Sorry, i can not find' + ' ' + id;
+          $scope.messages.push(message);
+        }
       }
     }
+
     $scope.content = '';
   };
 
@@ -72,6 +100,16 @@ angular.module('starter.controllers', [])
       return text.split('room')[1];
     } else if (text.indexOf('desk') !== -1) {
       return text.split('desk')[1];
+    } else if (text.indexOf('appoin') !== -1) {
+      var name = text.split('with')[1];
+      window.ws.send(JSON.stringify(
+          {'type': 'id',
+           'id': name}
+      ));
+      return null;
     }
   }
 });
+
+
+//make an appointment with yo
