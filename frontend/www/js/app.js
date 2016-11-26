ea6ff7c2429b7140be5cbffef141eca395ee43ba
liang <guoliang1222@gmail.com>
2016-11-26 13:34:33 +0200

    add reception bot demo

--- frontend/www/js/app.js
+++ frontend/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ionic.contrib.frostedGlass'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -52,7 +52,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   .state('tab.bot', {
     url: '/bot',
-    view: {
+    views: {
       'tab-bot': {
         templateUrl: 'templates/tab-bot.html',
         controller: 'BotCtrl'
